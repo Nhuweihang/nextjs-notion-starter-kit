827641a5ba02baa959a8686a5ee70435e9a42b7c
Weiang <61575199+Nhuweihang@users.noreply.github.com>
2022-08-06 22:39:12 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '79df143683da4d80b726cb56a8e98a9d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.notion.so/79df143683da4d80b726cb56a8e98a9d',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
